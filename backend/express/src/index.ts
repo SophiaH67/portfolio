30ffa539c8976f379922763b2b9233a5f53c48c6
marnixah <marnix@hagehoeve.com>
2021-09-15 22:30:01 +0200

    feat(backend/express): added skeleton

---
+++ backend/express/src/index.ts
@@ -0,0 +1,12 @@
+import express, { Application, Request, Response } from "express"
+
+const app: Application = express()
+const port = 3000
+
+app.get('/', (req: Request, res: Response)=> {
+  res.send(`Hello ${req.ip}`)
+}) 
+
+app.listen(port,()=>{
+  console.log(`Running on port ${port}`)
+});
