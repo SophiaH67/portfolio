b9a914734423cc2ac96b385dc0dafbcd4389fe64
Sophia Hage <sophia@sophiah.gay>
2021-09-16 23:23:24 +0200

    feat(frontend): make sections from array

---
+++ frontend/pages/components/sections.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import StoryInterface from '../interfaces/story'
+import Section from './section'
+import Story from './story'
+
+export default function Sections() {
+  const [stories, setStories] = useState<StoryInterface[]>([])
+  useEffect(() => {
+    console.log('Fetching api data...')
+    setStories(
+      Array.from(Array(10).keys()).map((n) => ({
+        title: `Title${n}`,
+        description: `Long description about ${n} and it's functionality`,
+      }))
+    )
+  }, [])
+  console.log(stories)
+  return (
+    <div>
+      {stories.map((story, i) => (
+        <Section alt={i % 2 == 1}>
+          <Story
+            key={i}
+            darkBackground={i % 2 == 1}
+            title={story.title}
+            text={story.description}
+          />
+        </Section>
+      ))}
+    </div>
+  )
+}
--- frontend/pages/components/story.tsx
+++ frontend/pages/components/story.tsx
@@ -6,9 +6,9 @@ interface Props {
 
 export default function Story({ darkBackground, text, title }: Props) {
   return ( 
-    <div className="text-center mx-auto max-w-5xl">
-      <h2 className={ (darkBackground ? 'text-white' : 'text-black') + " text-2xl font-semibold"}>{title}</h2>
-      <p className={ (darkBackground ? 'text-gray-300' : 'text-black') }>{text}</p>
+    <div className="text-center mx-auto max-w-5xl pt-20 pb-20">
+      <h2 className={ (darkBackground ? 'text-white' : 'text-gray-800') + " text-2xl font-semibold"}>{title}</h2>
+      <p className={ (darkBackground ? 'text-gray-300' : 'text-gray-800') }>{text}</p>
     </div>
    )
 }
