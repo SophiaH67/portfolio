3a2e6749cc54d42e433c5a0d1edc1ca1ea82edc9
Sophia Hage <sophia@sophiah.gay>
2021-09-17 14:55:44 +0200

    feat(backend/express): basic database mgmt

--- backend/express/src/index.ts
+++ backend/express/src/index.ts
@@ -1,12 +1,24 @@
 import express, { Application, Request, Response } from "express"
+import { Story } from './classes/story'
+import { sequelize } from "./db"
+
 require('dotenv').config()
 
 const app: Application = express()
 const port = 3000
 
-app.get('/', (req: Request, res: Response)=> {
-  res.send(`Hello ${req.ip}`)
-}) 
+app.get('/getStories', async (_req: Request, res: Response)=> {
+  res.send(await Story.findAll())
+})
+
+// DEV: to test database
+app.get('/addOne', async (_req: Request, res: Response) => {
+  const i = Math.random()
+  const newStory = await Story.create({title: `Some Title${i}`, description: `Some long *markdown* enabled description about a story with the title of ${i}`})
+  await newStory.save()
+  await sequelize.sync()
+  res.send(newStory).status(200)
+})
 
 app.listen(port,()=>{
   console.log(`Running on port ${port}`)
