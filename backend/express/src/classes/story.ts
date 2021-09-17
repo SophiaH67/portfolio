e4a09345037b96707a014ad2438e68a0a0ed5a38
marnix <marnix@hagehoeve.com>
2021-09-17 14:55:44 +0200

    feat(backend/express): basic database mgmt

---
+++ backend/express/src/classes/story.ts
@@ -0,0 +1,10 @@
+import { Model, DataTypes } from 'sequelize'
+import { sequelize } from '../db'
+
+export class Story extends Model {}
+
+Story.init({
+  title: DataTypes.STRING,
+  description: DataTypes.STRING,
+}, { sequelize, modelName: 'story' })
+sequelize.sync()
