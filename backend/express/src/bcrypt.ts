9e01eb2388d743f5bbb8a264c5b0f74f279f69bc
Sophia Hage <sophia@sophiah.gay>
2021-09-19 11:27:56 +0200

    dev(backend/express): added prettier

--- backend/express/src/bcrypt.ts
+++ backend/express/src/bcrypt.ts
@@ -1,39 +1,43 @@
-import { hash, genSalt } from "bcryptjs"
-import { Value } from "./classes/value"
+import { hash, genSalt } from 'bcryptjs'
+import { Value } from './classes/value'
 import safeCompare from 'safe-compare'
 
 let saltCache = ''
 
 const getSalt = async () => {
-  if(saltCache) return saltCache
-  const result = (await Value.findOne({ where: { key: "SALT" } }))
-  if (result) {saltCache = result.value; return saltCache}
+  if (saltCache) return saltCache
+  const result = await Value.findOne({ where: { key: 'SALT' } })
+  if (result) {
+    saltCache = result.value
+    return saltCache
+  }
 
-  const salt = await Value.create({key: 'SALT', value: await genSalt()})
+  const salt = await Value.create({ key: 'SALT', value: await genSalt() })
   await salt.save()
   saltCache = salt.value
   return saltCache
 }
 
 export const getPasswordHash = async () => {
-  const result = (await Value.findOne({ where: { key: "PASSWORD_HASH" } }))
+  const result = await Value.findOne({ where: { key: 'PASSWORD_HASH' } })
   return result?.value || ''
 }
 
 export const setPasswordHash = async (passwordHash: string) => {
-  if(await getPasswordHash()) {
-    const result = (await Value.findOne({ where: { key: "PASSWORD_HASH" } }))
-    if(!result) return // make TS shut up
+  if (await getPasswordHash()) {
+    const result = await Value.findOne({ where: { key: 'PASSWORD_HASH' } })
+    if (!result) return // make TS shut up
     result.value = passwordHash
     await result.save()
   } else {
-    const hash = await Value.create({key: 'PASSWORD_HASH', value: passwordHash})
+    const hash = await Value.create({
+      key: 'PASSWORD_HASH',
+      value: passwordHash,
+    })
     await hash.save()
   }
 }
 
-export const validatePasswordHash = async (passwordHash: string) => 
-  safeCompare(passwordHash, await getPasswordHash());
+export const validatePasswordHash = async (passwordHash: string) => safeCompare(passwordHash, await getPasswordHash())
 
-export const hashPassword = async (password: string) => 
-  hash(password, await getSalt())
+export const hashPassword = async (password: string) => hash(password, await getSalt())
--- backend/express/src/classes/value.ts
+++ backend/express/src/classes/value.ts
@@ -10,7 +10,7 @@ Value.init(
   {
     key: {
       type: DataTypes.STRING,
-      unique: true
+      unique: true,
     },
     value: DataTypes.STRING,
   },
