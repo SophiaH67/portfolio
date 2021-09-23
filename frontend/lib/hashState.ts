d312b3bee7e822db04728fffd603e2f5d5d91256
Sophia Hage <sophia@sophiah.gay>
2021-09-23 14:47:57 +0200

    fix(frontend): validateHash spam

--- frontend/lib/hashState.ts
+++ frontend/lib/hashState.ts
@@ -2,5 +2,12 @@
 export const getHash = () => 
   localStorage.getItem('hash') || ''
 
-export const setHash = (hash: string) =>
-  localStorage.setItem('hash', hash)
+export const setHash = (hash: string) => {
+  listeners.forEach(async (listener) => listener())
+  return localStorage.setItem('hash', hash)
+}
+
+let listeners: (()=>void)[] = []
+export const onHashChange = (callback: ()=>void) => {
+  listeners.push(callback)
+}
