bc0ee4abab4bebfe670f78a88aeca362728c5ca8
Sophia Hage <sophia@sophiah.gay>
2021-09-28 16:18:29 +0200

    fix(frontend): wrong use of anchor tag

--- frontend/components/bar.tsx
+++ frontend/components/bar.tsx
@@ -1,6 +1,7 @@
 import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from 'next/link'
 
 export default function Bar() {
   return (
@@ -8,7 +9,7 @@ export default function Bar() {
       <header className='bg-white shadow border-t-4 border-purple-700'>
         <div className='container mx-auto px-6 py-4'>
           <div className='flex items-center justify-between'>
-            <div className='text-black'><a href="/">Marnix</a></div>
+            <div className='text-black'><Link href="/">Marnix</Link></div>
             <div className="flex flex-row-reverse">
               <div className="mx-4">
                 <a href={`https://github.com/marnixah`}>
