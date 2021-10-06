4f7cdb79d3f341571856ae63014370d2d9bb9f06
marnixah <marnix@hagehoeve.com>
2021-10-06 21:31:36 +0200

    feat: init laravel
    Moved away from Express.JS to PHP Laravel

--- frontend/components/input.tsx
+++ frontend/components/input.tsx
@@ -1,21 +1,26 @@
 interface Props {
   state: [string, (value: string) => void]
   type?: string
+  placeholder: string
+  description: string
 }
 
-export default function Input({state, type} : Props) {
+export default function Input({state, type, placeholder, description} : Props) {
   const [value, setValue] = state
 
   return (
-    <input
-      onInput={(e) => {
-        const element = e.target as HTMLInputElement
-        setValue(element.value)
-      }}
-      placeholder='password'
-      className='min-w-full border-2 border-gray-300 rounded-md my-1 focus:outline-none'
-      type={type}
-      value={value}
-    ></input>
+    <div>
+      <label className="font-light text-gray-500 text-xs">{description}</label>
+      <input
+        onInput={(e) => {
+          const element = e.target as HTMLInputElement
+          setValue(element.value)
+        }}
+        placeholder={placeholder}
+        className='min-w-full border-2 border-gray-300 rounded-md p-1 my-1 focus:outline-none'
+        type={type}
+        value={value}
+      ></input>
+    </div>
   )
 }
