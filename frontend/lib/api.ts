97fd1371b0fe3b63d685ac143b2a3486104c4d39
Sophia Hage <sophia@sophiah.gay>
2021-10-09 01:22:16 +0200

    fix(frontend): moved backend base to function

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,4 +1,7 @@
 import axios from "axios"
 
+export const getBackendBase = () =>
+  process.env.NODE_ENV == 'production' ? '/' : 'http://localhost:8000'
+
 export const deleteMessage = (id: number) => 
-  axios.delete(`http://localhost:8000/api/messages/${id}`)
+  axios.delete(`${getBackendBase()}/api/messages/${id}`)
