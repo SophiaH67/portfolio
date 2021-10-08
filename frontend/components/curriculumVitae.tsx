871750bce40dd86b643eb43e3a200862720ff7b0
marnixah <marnix@hagehoeve.com>
2021-10-09 01:11:50 +0200

    Features/contactform (#1)
    * feat(frontend): moved navbar to _app.js
    
    * feat(frontend): repetitive divs into components
    
    * feat: contact form on main page
    
    * feat(frontend): messageCard component
    
    This component is made for the admin to view all the cards
    
    * feat(frontend): className prop in messageCard
    
    This is to support customizable margins and stuff
    
    * feat(frontend): moved message props into interface
    
    * feat(frontend): admin messages page
    
    * feat(frontend): removal of messages
    
    * feat(backend): removal of messages
    
    * chore(frontend): removed unused imports

--- frontend/components/curriculumVitae.tsx
+++ frontend/components/curriculumVitae.tsx
@@ -1,26 +1,31 @@
 import Button from './button'
+import FloatingCard from './floatingCard'
 
 export default function CurriculumVitae() {
   return (
-    <div className='min-h-min pt-24 flex items-center pb-32 w-full'>
-      <div className='mx-auto max-w-full w-11/12 md:w-auto border-gray-100 bg-white pt-4 md:p-4 border-2 rounded-md'>
-        <h2 className='text-center min-w-min text-6xl font-semibold text-gray-800 pb-4'>About Me</h2>
-        <div className='flex flex-col md:flex-row'>
-          <img
-            className='w-full md:w-auto md:max-w-sm md:pr-2'
-            src='https://picsum.photos/512/512'
-          />
-          <div className="p-1 md:p-0 md:flex md:flex-col md:justify-between">
-            <p className='max-w-sm'>
-              Beeg paragraph describing a lot of stuff that I have done and
-              stuff yeah
-            </p>
-            <div className="flex justify-center">
-              <Button className='bottom-0 mx-2 w-full md:w-auto'>Download CV</Button>
-            </div>
+    <FloatingCard>
+      <>
+      <h2 className='text-center min-w-min text-6xl font-semibold text-gray-800 pb-4'>
+        About Me
+      </h2>
+      <div className='flex flex-col md:flex-row'>
+        <img
+          className='w-full md:w-auto md:max-w-sm md:pr-2'
+          src='https://picsum.photos/512/512'
+        />
+        <div className='p-1 md:p-0 md:flex md:flex-col md:justify-between'>
+          <p className='max-w-sm'>
+            Beeg paragraph describing a lot of stuff that I have done and stuff
+            yeah
+          </p>
+          <div className='flex justify-center'>
+            <Button className='bottom-0 mx-2 w-full md:w-auto'>
+              Download CV
+            </Button>
           </div>
         </div>
       </div>
-    </div>
+      </>
+    </FloatingCard>
   )
 }
--- frontend/pages/auth.tsx
+++ frontend/pages/auth.tsx
@@ -1,9 +1,7 @@
 import React from "react"
 import Auth from '../components/auth'
-import Bar from '../components/bar'
 
 export default function Home() {
   return <div>
-    <Bar></Bar>
     <Auth></Auth></div>
 }
