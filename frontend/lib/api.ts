f065cfc02311d0208696fc7cd17eeed7b8a02074
marnixah <marnix@hagehoeve.com>
2021-10-09 12:33:30 +0200

    fix(frontend): absolute backend base

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 export const getBackendBase = () =>
-  process.env.NODE_ENV == 'production' ? '/' : 'http://localhost:8000'
+  process.env.NODE_ENV == 'production' ? window.location.href.split('/').slice(0,3).join('/') : 'http://localhost:8000'
 
 export const deleteMessage = (id: number) => 
   axios.delete(`${getBackendBase()}/api/messages/${id}`)
