d89484e769675587a7c8ac1d47a921280085ae19
Sophia Hage <sophia@sophiah.gay>
2021-10-11 08:51:18 +0200

    feat(frontend): consistent card styling

--- frontend/components/floatingCard.tsx
+++ frontend/components/floatingCard.tsx
@@ -3,7 +3,7 @@ interface Props {
 }
 
 export default function floatingCard({children}: Props) {
-  return <div className='w-11/12 max-w-4xl md:w-auto border-gray-100 bg-white pt-4 md:p-4 border-2 rounded-md mx-auto'>
+  return <div className='w-11/12 max-w-4xl md:w-auto bg-white pt-4 md:p-4 shadow-xl rounded-lg mx-auto'>
     {children}
   </div>
 }
