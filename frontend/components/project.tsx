7364f0475962f1040a36ebea97a3a470c614de5a
marnixah <marnix@hagehoeve.com>
2021-10-11 08:51:18 +0200

    feat(frontend): consistent card styling

--- frontend/components/project.tsx
+++ frontend/components/project.tsx
@@ -6,7 +6,7 @@ interface Props {
 
 export default function Project({ project }: Props) {
   return (
-    <div className='border-gray-200 border-2 bg-gray-100 w-11/12 my-4 md:max-w-min rounded-md md:m-4'>
+    <div className='shadow-xl rounded-lg bg-gray-100 w-11/12 my-4 md:max-w-min md:m-4'>
       <img src={project.image} className="w-full" />
       <h3 className="text-2xl text-gray-800 font-normal px-1">{project.title}</h3>
       <p className="min-w-max max-w-3xl font-light px-1 text-gray-700">{project.description}</p>
