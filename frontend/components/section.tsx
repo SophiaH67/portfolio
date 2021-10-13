2cdc2627dec42e7fb16fae28719115d383eae2b4
marnixah <marnix@hagehoeve.com>
2021-10-13 13:19:27 +0200

    fix(frontend): properly center items in Sections

--- frontend/components/section.tsx
+++ frontend/components/section.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 export default function Section({ children }: Props) {
   return (
-    <div className='min-h-min pt-24 flex items-center pb-24 mx-auto max-w-full w-full md:w-auto first:mx-auto'>
+    <div className='min-h-min pt-24 flex items-center justify-around pb-24 mx-auto max-w-full w-full md:w-auto first:mx-auto'>
       {children}
     </div>
   )
