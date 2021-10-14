37eb065d476a860e2d5decd43daf8313c4f75406
Sophia Hage <sophia@sophiah.gay>
2021-10-14 09:26:04 +0200

    feat(frontend): getStaticProps for projects on /

--- frontend/components/projects.tsx
+++ frontend/components/projects.tsx
@@ -4,40 +4,35 @@ import { getProjects } from '../lib/api'
 import Loading from './loading'
 import ProjectCard from './projectCard'
 
-export default function Projects() {
-  const [projects, setProjects] = useState<ProjectInterface[]>([])
-  const [fetching, setFetching] = useState(true)
+export interface ProjectsProps {
+  initialProjects: ProjectInterface[]
+}
+
+export default function Projects({ initialProjects }: ProjectsProps) {
+  const [projects, setProjects] = useState<ProjectInterface[]>(initialProjects)
 
   useEffect(() => {
-    getProjects()
-      .then(setProjects)
-      .then(() => setFetching(false))
+    getProjects().then(setProjects)
   }, [])
 
   return (
     <div className='min-h-min bg-gray-800 pb-20 block w-full'>
       <h2 className='pt-24 pb-12 text-gray-100 font-semibold mx-auto max-w-min text-6xl'>Projects</h2>
-      {fetching ? (
-        <div className='flex justify-center'>
-          <Loading size='70px' />
+      <div className='w-full mx-auto'>
+        <div className='flex flex-wrap justify-center'>
+          {projects.map((project) => (
+            <a href={project.link} target='_blank' rel='noreferrer' key={project.id}>
+              <ProjectCard
+                id={project.id}
+                className='my-2 mx-4'
+                name={project.name}
+                description={project.description}
+                link={project.link}
+              />
+            </a>
+          ))}
         </div>
-      ) : (
-        <div className='w-full mx-auto'>
-          <div className='flex flex-wrap justify-center'>
-            {projects.map((project) => (
-              <a href={project.link} target='_blank' rel='noreferrer' key={project.id}>
-                <ProjectCard
-                  id={project.id}
-                  className='my-2 mx-4'
-                  name={project.name}
-                  description={project.description}
-                  link={project.link}
-                />
-              </a>
-            ))}
-          </div>
-        </div>
-      )}
+      </div>
     </div>
   )
 }
