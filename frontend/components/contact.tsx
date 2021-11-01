b9d25912c0fd3fba7d5d6e0e39386b6132b76871
Sophia Hage <sophia@sophiah.gay>
2021-11-01 09:27:05 +0100

    fix: remove deprecated header set

--- frontend/components/contact.tsx
+++ frontend/components/contact.tsx
@@ -27,7 +27,6 @@ export default function Contact() {
     }
 
     axios.defaults.withCredentials = true
-    axios.defaults.headers = { accept: 'application/json' }
 
     axios
       .post(`${getBackendBase()}/api/messages`, body, {
