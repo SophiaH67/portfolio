7be3f461bb976a62bdf9fea138eb723364ac8279
Sophia Hage <sophia@sophiah.gay>
2021-11-13 13:43:29 +0100

    fix: aboutme showing with weird spacing

--- frontend/components/curriculumVitae.tsx
+++ frontend/components/curriculumVitae.tsx
@@ -15,13 +15,8 @@ export default function CurriculumVitae({ aboutme }: CurriculumVitaeProps) {
         <div className='flex flex-col md:flex-row'>
           <img className='w-full md:w-auto md:max-w-sm md:pr-2' src='https://picsum.photos/512/512' />
           <div className='p-1 md:p-0 md:flex md:flex-col md:justify-between'>
-            <p className='max-w-sm'>
-              {aboutme.split('\n').map((line,i) => (
-                <span key={i}>
-                  {line}
-                  <br />
-                </span>
-              ))}
+            <p className='w-full'>
+              {aboutme}
             </p>
             <div className='flex justify-center'>
               <Button className='bottom-0 mx-2 w-full md:w-auto'>Download CV</Button>
