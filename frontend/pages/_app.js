7499291947ffd8f6992939b35d9734fc7bff6672
Sophia Hage <sophia@sophiah.gay>
2021-11-13 16:25:45 +0100

    feat: set lang to nl when browser lang is nl

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -3,8 +3,11 @@ import '../styles/background.css'
 import 'tailwindcss/tailwind.css'
 import Bar from '../components/bar'
 import Head from 'next/head'
+import { isNL } from '../lib/locale'
 
 function MyApp({ Component, pageProps }) {
+  if (isNL()) 
+    document.getElementsByTagName("html")[0].lang = 'nl'
   return (
     <div className='font-sans'>
       <Head>
